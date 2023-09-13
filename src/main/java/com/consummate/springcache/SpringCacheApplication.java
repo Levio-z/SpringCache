56de8a6d204f74322c08df4e78a72ed876089b3a
consummate <2779188830@qq.com>
2023-09-13 18:02:53 +0800

    1

--- src/main/java/com/consummate/springcache/SpringCacheApplication.java
+++ src/main/java/com/consummate/springcache/SpringCacheApplication.java
@@ -1,13 +1,31 @@
 package com.consummate.springcache;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
-public class SpringCacheApplication {
+@RequiredArgsConstructor
+public class SpringCacheApplication implements ApplicationListener<ApplicationReadyEvent> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCacheApplication.class, args);
-	}
+    private final RedisTemplate redisTemplate;
 
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        System.out.println("应用程序已准备就绪");
+        redisTemplate.opsForValue().set("key","1");
+        stringRedisTemplate.opsForValue().set("key","value");
+        System.out.println("redis正常访问：key：key,value:"+redisTemplate.opsForValue().get("key"));
+        System.out.println("redis正常访问：key：key,value:"+stringRedisTemplate.opsForValue().get("key"));
+    }
 }
